8066b8ed2c06ea677b114224f4a201827df2da8d
megicula27 <aditya1272001@gmail.com>
2024-12-14 21:27:42 +0530

    added user model temp

---
+++ src/models/User/User.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+
+const UserSchema = new mongoose.Schema(
+  {
+    uid: { type: String, required: true },
+    username: { type: String, unique: true },
+    email: { type: String, unique: true },
+    password: { type: String },
+    avatar: String,
+
+    activeStatus: { type: Boolean, default: true },
+    createdAt: { type: Date, default: Date.now },
+  },
+  {
+    _id: true,
+  }
+);
+
+export default mongoose.models.User || mongoose.model("User", UserSchema);
